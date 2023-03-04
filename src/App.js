89b457b3cc480554c659504ba5b10093940b454d
EmoHei <98215816+EmoHei@users.noreply.github.com>
2023-03-04 14:35:28 +0100

    created -> home,login,register,forgot-password pages & routes

--- src/App.js
+++ src/App.js
@@ -1,10 +1,25 @@
-
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import './App.css';
+import ForgotPassword from './pages/forgotPassword/ForgotPassword';
+import Home from './pages/home/Home';
+import Login from './pages/login/Login';
+import Register from './pages/register/Register';
 
 function App() {
   return (
-    <div className="container-fluid bg-primary" >
-     <h1>New Project</h1>
+    <div className="container-fluid bg-primary   d-flex justify-content-center" >
+
+      <Router>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Register/>} />
+          <Route path="/forgot-password" element={<ForgotPassword />} />
+        </Routes>
+
+      </Router>
+
     </div>
   );
 }
